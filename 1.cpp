88ac355910a23d464510a51201e0994dcc3012cd
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-02-18 12:51:47 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
- 
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -10,13 +10,13 @@ int main()
     cin >> n;
     cout << "Введите число столбцов матрицы: " << endl;
     cin >> m;
-    int *imin = new int [n];            //номера столбцов минимальных элементов в каждой строке
-    double** arr = new double*[n];      //двумерный массив! Сначала получаем массив адресов строк. Кроме того, матрица из double, а не int
+    int* imin = new int[n];            //номера столбцов минимальных элементов в каждой строке
+    double** arr = new double* [n];     
     cout << "элементы: " << endl;
     for (i = 0; i < n; ++i)
     {
-        arr[i] = new double[m];         //строка
-        for (j = 0; j < m; ++j)         //вводим строку
+        arr[i] = new double[m];         
+        for (j = 0; j < m; ++j)         
         {
             cout << "arr[" << i << "][" << j << "] = ";
             cin >> arr[i][j];
@@ -27,32 +27,32 @@ int main()
     for (i = 0; i < n; ++i)
     {
         for (j = 0; j < m; ++j)
-            cout << arr[i][j] << '\t';
+            cout << *( *(arr+i) +j) << '\t';
         cout << endl;
     }
-    for (i = 0; i < n; ++i)             //найдем минимальные элементы в каждой строке
+    for (i = 0; i < n; ++i)             //поиск минимального элемента в каждой строке
     {
-        imin[i] = 0;                    //сначала номер столбца пусть = 0
-        for (j = 1; j < m; ++j)         //с 1 до конца строки
+        imin[i] = 0;                  
+        for (j = 1; j < m; ++j)         
         {
-            if (arr[i][j] < arr[i][imin[i]])
-                imin[i] = j;            //новый номер столбца с минимальным элементом
+            if (*(*(arr+i)+j) < *( *(arr+i)+ imin[i]))
+                imin[i] = j;            
         }
     }
- 
-    for (i = 0; i < n - 1; ++i)         //сортируем полученный массив
+
+    for (i = 0; i < n - 1; ++i)         //сортировка полученного массива
     {
-        for (j = i +1; j < n; ++j)
+        for (j = i + 1; j < n; ++j)
         {
-            if (arr[i][imin[i]] > arr[j][imin[j]])  //в строках сравниваем только элементы в столбцах с минимальным элементом
+            if ( *(*(arr+i) + imin[i]) > *(*(arr+j)+imin[j]))  
             {
-                for (k = 0; k < m; ++k)             //меняем местами две строки
+                for (k = 0; k < m; ++k)             //замена мест строк
                 {
-                    tmp = arr[i][k];
-                    arr[i][k] = arr[j][k];
-                    arr[j][k] = tmp;
+                    tmp = *(*(arr+i)+k);
+                    *(*(arr+i)+k) = *(*(arr + j) + k);
+                    *(*(arr + j) + k) = tmp;
                 }
-                itmp = imin[i];                 //и номера столбцов с минимальным элементом, т.к. строки поменены местами
+                itmp = imin[i];                 //замена номера столбцов с минимальным элементом
                 imin[i] = imin[j];
                 imin[j] = itmp;
             }
@@ -65,11 +65,11 @@ int main()
             cout << arr[i][j] << '\t';
         cout << endl;
     }
-    for (i = 0; i < n; ++i)             //удаляем запрошенную память
-        delete[] arr[i];                //сначала строки
-    delete[] arr;                       // затем и массив адресов строк
+    for (i = 0; i < n; ++i)             
+        delete[] arr[i];                
+    delete[] arr;                       
     delete[] imin;
- 
+
     system("pause");
     return 0;
 }
