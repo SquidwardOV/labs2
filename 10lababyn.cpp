c6835747f2668bc804dba89b2012c019d63922bf
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-06-03 12:46:07 +0300

    Update 10lababyn.cpp

--- 10lababyn.cpp
+++ 10lababyn.cpp
@@ -9,7 +9,7 @@ struct FileInfo {
     char day[4];
     char month[4];
     char year[6];
-    char size[10];
+    char size[14];
 };
 
 bool sr(char arr1[3], char arr2[3]) {
@@ -20,6 +20,7 @@ bool sr(char arr1[3], char arr2[3]) {
     }
     return true;
 }
+
 void remove(std::string file_name, std::string file_name_bin) {
     std::ifstream file(file_name, std::ios::binary | std::ios::in);
     std::ofstream file_bin(file_name_bin, std::ios::binary | std::ios::out);
@@ -28,6 +29,19 @@ void remove(std::string file_name, std::string file_name_bin) {
         while (!file.eof()) {
             file.read((char*)&tx, sizeof(tx));
             file_bin.write((char*)&tx, sizeof(tx));
+        }
+    }
+}
+void result(std::string file_name_bin,char base[5]) {
+    std::ifstream file_bin(file_name_bin, std::ios::binary | std::ios::in);
+    std::ofstream file_result("result.bin", std::ios::binary | std::ios::out);
+    FileInfo tx;
+    if (file_bin.is_open()) {
+        while (!file_bin.eof()) {
+            file_bin.read((char*)&tx, sizeof(tx));
+            if (sr(base, tx.extension)) {
+                file_result.write((char*)&tx, sizeof(tx));
+            }
         }
     }
 }
@@ -40,19 +54,18 @@ int main() {
     char base[5];
     int score = 0;
     FileInfo tx;
+    char t;
     std::cout << "Введите нужное разрешение:" << std::endl;
     std::cin >> base;
     base[3] = '\r';
     base[4] = '\n';
     remove(file_name, file_name_bin);
-    std::ifstream file_bin(file_name_bin, std::ios::binary | std::ios::in);
-    std::ofstream file_result("result.bin", std::ios::binary | std::ios::out);
-    if (file_bin.is_open()) {
-        while (!file_bin.eof()) {
-            file_bin.read((char*)&tx, sizeof(tx));
-            if (sr(base, tx.extension)) {
-                file_result.write((char*)&tx, sizeof(tx));
-            }
+    result(file_name_bin, base);
+    std::ifstream file_result("result.bin", std::ios::binary | std::ios::in);
+    if (file_result.is_open()) {
+        while (!file_result.eof()) {
+            file_result.read((char*)&t, sizeof(t));
+            std::cout << t;
         }
     }
 }
